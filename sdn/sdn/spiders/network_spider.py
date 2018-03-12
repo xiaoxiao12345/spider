c9ce9f46e3c94ddb961b4e6cfe42e190b328978a
xiaoxiao <1136283883@qq.com>
2018-03-12 17:18:25 +0800

    can get jiagou.html

---
+++ sdn/sdn/spiders/network_spider.py
@@ -0,0 +1,11 @@
+import scrapy
+
+class NetworkSpider(scrapy.Spider):
+    name = "network"
+    start_urls=[
+        "http://www.sdnlab.com/"
+    ]
+    def parse(self, response):
+        filename = 'jiagou.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
