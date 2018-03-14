7cc94c573bb73e20a37e99f8c2b61494e5e17c97
xiaoxiao <1136283883@qq.com>
2018-03-14 18:42:49 +0800

    问题在于怎么找到要的观点,xpath要怎么设置

--- sdn/sdn/settings.py
+++ sdn/sdn/settings.py
@@ -49,9 +49,10 @@ NEWSPIDER_MODULE = 'sdn.spiders'
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'sdn.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'sdn.middlewares.MyCustomDownloaderMiddleware': 543,
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
